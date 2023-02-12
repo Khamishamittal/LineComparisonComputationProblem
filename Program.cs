5250504adcb6971ebb69679f898e7c51cd25e788
“Khamishamittal” <Khamishamittal090@gmail.com>
2023-02-12 14:03:27 +0530

    Welcome to line comparison Problem

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LineComparisonComputation
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Line Comparison Computation Program");
+        }
+    }
+}
